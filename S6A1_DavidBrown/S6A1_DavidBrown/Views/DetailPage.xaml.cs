705c9dcfb539f90732f39df180fa717a70cd0dad
dabrown104 <raven122685@yahoo.com>
2016-06-17 23:45:40 -0400

    Assignment Seminar 6
    S6A1 Assignment

---
+++ S6A1_DavidBrown/S6A1_DavidBrown/Views/DetailPage.xaml.cs
@@ -0,0 +1,16 @@
+using S6A1_DavidBrown.ViewModels;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+
+namespace S6A1_DavidBrown.Views
+{
+    public sealed partial class DetailPage : Page
+    {
+        public DetailPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = NavigationCacheMode.Disabled;
+        }
+    }
+}
+
---
+++ S6A1_DavidBrown/S6A1_DavidBrown/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+using System;
+using S6A1_DavidBrown.ViewModels;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+using System.Collections.ObjectModel;
+
+namespace S6A1_DavidBrown.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            NavigationCacheMode = Windows.UI.Xaml.Navigation.NavigationCacheMode.Enabled;
+        }
+    }
+}
